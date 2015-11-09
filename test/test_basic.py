6ee6838aa9ad0c44cd2200506290269fbe3d2b79
fitnr <fitnr@fakeisthenewreal>
2015-11-09 01:12:36 -0500

    Update tests, wrap in unittest class
    3.5 is out

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,35 +1,50 @@
-import random
+import unittest
 import markovify
 import sys, os
 import operator
 
-HERE = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(HERE, "texts/sherlock.txt")) as f:
-    sherlock = f.read()
-
-def test_text_too_small():
-    text = u"Example phrase. This is another example sentence."
-    text_model = markovify.Text(text)
-    assert(text_model.make_sentence() == None)
-
-def test_sherlock():
-    text_model = markovify.Text(sherlock)
-    sent = text_model.make_sentence()
-    assert(len(sent) != 0)
-
 def get_sorted(chain_json):
     return sorted(chain_json, key=operator.itemgetter(0))
 
-def test_json():
-    text_model = markovify.Text(sherlock)
-    chain_json = text_model.chain.to_json()
-    stored_chain = markovify.Chain.from_json(chain_json)
-    assert(get_sorted(stored_chain.to_json()) == get_sorted(chain_json))
-    new_text_model = markovify.Text(sherlock, chain=stored_chain)
-    sent = text_model.make_sentence()
-    assert(len(sent) != 0)
+class MarkovifyTest(unittest.TestCase):
 
-def test_make_sentence_with_start():
-    text_model = markovify.Text(sherlock)
-    sent = text_model.make_sentence_with_start("Sherlock Holmes")
-    assert(sent != None)
+    def setUp(self):
+        with open(os.path.join(os.path.dirname(__file__), "texts/sherlock.txt")) as f:
+            self.sherlock = f.read()
+
+    def test_text_too_small(self):
+        text = u"Example phrase. This is another example sentence."
+        text_model = markovify.Text(text)
+        assert(text_model.make_sentence() == None)
+
+    def test_sherlock(self):
+        text_model = markovify.Text(self.sherlock)
+        sent = text_model.make_sentence()
+        assert(len(sent) != 0)
+
+    def test_json(self):
+        text_model = markovify.Text(self.sherlock)
+
+        sent = text_model.make_sentence()
+        assert(len(sent) != 0)
+
+    def test_chain(self):
+        text_model = markovify.Text(self.sherlock)
+        chain_json = text_model.chain.to_json()
+
+        stored_chain = markovify.Chain.from_json(chain_json)
+        assert(get_sorted(stored_chain.to_json()) == get_sorted(chain_json))
+
+        new_text_model = markovify.Text.from_chain(chain_json)
+        assert(get_sorted(new_text_model.chain.to_json()) == get_sorted(chain_json))
+
+        sent = new_text_model.make_sentence()
+        assert(len(sent) != 0)
+
+    def test_make_sentence_with_start(self):
+        text_model = markovify.Text(self.sherlock)
+        sent = text_model.make_sentence_with_start("Sherlock Holmes")
+        assert(sent != None)
+
+if __name__ == '__main__':
+    unittest.main()

5044c01ea1d4c23f7531cdfde0e53ab4a462efd6
Jeremy Singer-Vine <jsvine@gmail.com>
2015-11-12 00:02:27 -0500

    Revert state_size-default and further absolutify

--- markovify/__init__.py
+++ markovify/__init__.py
@@ -1,6 +1,6 @@
 VERSION_TUPLE = (0, 2, 4)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
-from markovify.chain import Chain
-from markovify.text import Text
-from markovify.splitters import split_into_sentences
+from .chain import Chain
+from .text import Text
+from .splitters import split_into_sentences

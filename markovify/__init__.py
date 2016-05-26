6bbd618c2dc78931e392601473b8823297c5dd49
Jeremy Singer-Vine <jsvine@gmail.com>
2016-05-26 09:30:11 -0400

    Add NewlineText to main namespace, bump to v0.3.2

--- markovify/__init__.py
+++ markovify/__init__.py
@@ -1,7 +1,7 @@
-VERSION_TUPLE = (0, 3, 1)
+VERSION_TUPLE = (0, 3, 2)
 VERSION = ".".join(map(str, VERSION_TUPLE))
 
 from .chain import Chain
-from .text import Text
+from .text import Text, NewlineText
 from .splitters import split_into_sentences
 from .utils import combine

f8aa65e6bc185eb12be8c3aa4c7677e562e6d86d
Alex <asigalov61@hotmail.com>
2021-01-27 15:51:37 -0800

    Replace random with secrets.random

--- markovify/chain.py
+++ markovify/chain.py
@@ -1,4 +1,5 @@
 import random
+import secrets
 import operator
 import bisect
 import json
@@ -111,7 +112,8 @@ class Chain(object):
         else:
             choices, weights = zip(*self.model[state].items())
             cumdist = list(accumulate(weights))
-        r = random.random() * cumdist[-1]
+        # r = random.random() * cumdist[-1]
+        r = secrets.choice([random.random(), random.random()]) * cumdist[-1]
         selection = choices[bisect.bisect(cumdist, r)]
         return selection
 

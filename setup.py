b2546488f0ee69d8428aecc6ac807f4e67385a5d
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2021-01-28 21:12:39 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="markov chain text",
     author="Jeremy Singer-Vine",
